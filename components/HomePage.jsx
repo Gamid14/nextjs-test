d9b52d06340dbc4986d399d5b281baa332974ce1
Daervil <gamidik1999@mail.ru>
2024-03-13 13:34:33 +0300

    made some fixes

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -5,10 +5,10 @@ const HomePage = () => {
     <section className='max-w-xs md:max-w-md mx-auto text-center h-screen flex flex-col justify-center gap-8'>
       <h2 className='text-4xl font-bold'>About Me</h2>
       <p className='text-lg'>
-        Hi, I'm Denis – UX/UI designer from Minsk. I'm interested in design and everything connected with it.
+        Hi, I&apos;m Denis – UX/UI designer from Minsk. I&apos;m interested in design and everything connected with it.
       </p>
       <p className='text-lg'>
-        I'm studying at courses "Web and mobile design interfaces" in IT-Academy.
+        I&apos;m studying at courses &quot;Web and mobile design interfaces&quot; in IT-Academy.
       </p>
       <p className='text-lg'>
         Ready to implement excellent projects
--- components/SkillsImages.jsx
+++ components/SkillsImages.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image"
+import starsImage from '../public/stars.png'
 const SkillsImages = ({src, width, height, alt, description, priority}) => {
   return (
     <div className="grid grid-rows-[90px_1fr_auto] justify-items-center gap-y-6">
       <Image className="" src={src} width={width} height={height} alt={alt} priority={priority}/>
       <p className="text-[#828282] text-center font-semibold max-w-24">{description}</p>
-      <img src="/stars.png" alt="" />
+      <Image src={starsImage} alt="Rating stars" />
     </div>
   )
 }
